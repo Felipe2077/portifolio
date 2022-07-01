07e07cc4dcc6bdbcdc8073878db43d7eaef9b8ed
felipe2077 <felipebatista47@hotmail.com>
2022-07-01 10:18:41 -0300

    project area fixes

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
+//*funcao que faz o scroll suave
 function initScroll() {
+  //* pega o link interno clicado
   const itemMenu = document.querySelectorAll(".js-menu a[href^='#']");
   function scrollSection(element) {
     element.preventDefault();
@@ -9,6 +11,8 @@ function initScroll() {
       block: "start",
     });
   }
+
+  //*Laço que pega o click e executa a funcao de scroll
   itemMenu.forEach((element) => {
     element.addEventListener("click", scrollSection);
   });
